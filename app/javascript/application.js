950da6bc76057af8e7c2ca9d67e74cd1fe8f95a5
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-01-11 14:23:44 +0200

    Implement ajax

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+// import "jquery-ui/effect-blind";
+import "jquery";
+import "jquery_ujs";
+import "./jquery_ui";
