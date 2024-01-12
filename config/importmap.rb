0c969f0952e88121e8b16c25a79bbd0b868c3c5b
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-01-12 15:11:19 +0200

    Generate channel

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "jquery.min.js", preload: true
 pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin "jquery-ui", to: "jquery-ui.min.js", preload: true
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
