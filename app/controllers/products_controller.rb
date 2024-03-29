1f317c62622940683a50b0a520666fd0bce574eb
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-03-29 09:51:06 +0200

    Update products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,7 +71,7 @@ class ProductsController < ApplicationController
     # end 
   end
 
-  def who_brought
+  def who_bought
     @product = Product.find(params[:id])
     @latest_order = @product.orders.order(:updated_at).last
     if stale?(@latest_order)
