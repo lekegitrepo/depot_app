81e98ab8a4742507a53e0b20b4e4e908157424c7
Temitayo Adeleke <lekedevrepo@gmail.com>
2024-06-09 10:49:44 +0300

    Create line_items migration

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_14_131725) do
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "cart_id", null: false
+    t.integer "product_id"
+    t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
